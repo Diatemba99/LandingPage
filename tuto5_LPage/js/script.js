8fe3ad6a8f53178fb07e350661971e0b2e504784
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2020-04-19 00:11:52 +0000

    Add files via upload

---
+++ tuto5_LPage/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+function toggle() 
+{
+    var trailer = document.querySelector('.trailer');
+    var video = document.querySelector('.video');
+    trailer.classList.toggle('active');
+    video.currentTime = 0;
+    video.pause();
+}
